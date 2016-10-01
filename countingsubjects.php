1e51964466a7832e278d2f9e829312830fa0f895
shahedex <shahed739@gmail.com>
2016-10-01 22:30:31 +0600

    php file for counting and calculating the per.

--- countingsubjects.php
+++ countingsubjects.php
@@ -11,11 +11,13 @@ if($conn->connect_error){
     die("Connection failed : " .$conn->connect_error);
 }
 
-
 static $math231=0,$me211=0,$me231=0,$me251=0,$hum231=0;
 static $vote_number = 0;
 $names = array("sub1","sub2","sub3","sub4","sub5");
 
+static $percent_array = array();
+static $leading_subject = array();
+static $subject = array();
 for($x=0;$x<5;$x++){
     $sql_query = "SELECT * FROM subs";
     $result = $conn->query($sql_query);
@@ -26,12 +28,33 @@ for($x=0;$x<5;$x++){
             $total = $row["id"];
             countSub($row[$names[$x]]);
         }
-        calculate($total);
+        $percent_array[$x] = calculate($total);
+        $leading_subject[$x] = leaderboard();
         zero();
     }
     else 
     {
         echo "error ... 0 results";
+    }
+}
+// echo $percent_array[0];
+for($s=0;$s<5;$s++){
+    switch ($leading_subject[$s]){
+        case "me231" : 
+            $subject[$s] = "Engineering Mechanics - 1";
+            break;
+        case "me211" :
+            $subject[$s] = "Thermodynamics";
+            break;
+        case "me251" : 
+            $subject[$s] = "Production Process";
+            break;
+        case "hum231" : 
+            $subject[$s] = "Technical English & Communication Skill";
+            break;
+        case "math231" :
+            $subject[$s] = "Statistical Analysis & Complex Variable";
+            break;
     }
 }
 
@@ -58,10 +81,21 @@ function calculate($totals){
     
     global $math231,$me211,$me231,$me251,$hum231;
     $maximum_val = array("math231"=>"$math231","me211"=>"$me211","me231"=>"$me231","me251"=>"$me251","hum231"=>"$hum231");
-    $maxs = array_keys($maximum_val,  max($maximum_val));
+    // $maxs = array_keys($maximum_val,  max($maximum_val));
     $max_val = max($maximum_val);
     $percent = ($max_val / $totals) * 100;
-    echo "No one is : " .$maxs[0]."    with  ".$max_val."  points and ".number_format($percent,2)."% votes<br>";
+    // echo "No one is : " .$maxs[0]."    with  ".$max_val."  points and ".number_format($percent,2)."% votes<br>";
+    // echo number_format($percent,2)."%<br>";
+    return $percent;
+}
+
+function leaderboard(){
+    global $math231,$me211,$me231,$me251,$hum231;
+    
+    $maximum_val = array("math231"=>"$math231","me211"=>"$me211","me231"=>"$me231","me251"=>"$me251","hum231"=>"$hum231");
+    $maxs = array_keys($maximum_val,  max($maximum_val));
+
+    return $maxs[0];
 }
 function zero(){
         global $math231,$me211,$me231,$me251,$hum231;
